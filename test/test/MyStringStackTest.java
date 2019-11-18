b19f9ea11b6682de793c774eae1c2b121303ed6a
verdevops <verdevopst@gmail.com>
2019-11-18 23:40:17 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -32,6 +32,7 @@ public class MyStringStackTest extends TestCase {
 	@Test
 	public void testPush() {
 		String o1 = null;  
+		stack.push (s1);//noted,ver:added!
 		MyStringStack stack = new MyStringStack();
 		assertTrue (stack.isEmpty());
         stack.push (s1);
